2340b2f9eaaad8498a853ab5678ecc0d159e3c43
rarimuk <arimukruth@gmail.com>
2022-12-09 10:20:31 -0800

    Task 11 update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - prints all possible different combinations of three digits
  *
  * Return: Always 0 (Success)
  *
@@ -11,24 +11,30 @@ int main(void)
 {
 	int ones = 0;
 	int tens = 0;
+	int hundreds = '0';
 
-	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		for (tens = '0'; tens <= '9'; tens++)
 		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repetition*/
+			for (ones = '0'; ones <= '9'; ones++)
 			{
-				putchar(tens);
-				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
+				if (!((ones == tens) || (tens == hundreds) ||
+						(tens > ones) || (hundreds > tens))) /*eliminates repetition*/
 				{
-					putchar('.');
-					putchar(' ');
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' &&
+								tens == '8')) /*adds commas and spaces*/
+					{
+						putchar('.');
+						putchar(' ');
+					}
 				}
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
