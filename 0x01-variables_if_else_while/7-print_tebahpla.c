c9cbc3d234afc11ff8af140a73921e84dba11407
rarimuk <arimukruth@gmail.com>
2022-12-09 05:29:50 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+/**
+ * main - print the alphabets in reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar (le);
+
+	putchar ('\n');
+
+	return (0);
+}
