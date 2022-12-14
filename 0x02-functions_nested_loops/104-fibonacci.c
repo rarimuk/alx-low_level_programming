30afcb10ec475bd44c793d2fbae68d0977c035f8
rarimuk <arimukruth@gmail.com>
2022-12-14 15:15:21 -0800

    the first 98 Fibonacci numbers updates

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the first 98 fibonacci numbers, starting with
- * 1 and 2, separated by comma followed by a space.
+ * 1 and 2, separated by a comma followed by a space.
  * Return: Always 0
  */
 
@@ -45,4 +45,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
