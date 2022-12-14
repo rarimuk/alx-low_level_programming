d01e86bd19801fac6ed94dee82e57f5187fb5d11
rarimuk <arimukruth@gmail.com>
2022-12-14 03:03:13 -0800

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,13 +12,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i < = 0)
+	while (i <= 9)
 	{
-	for (c = 'a'; c < = 'z'; c++)
-	}
+	for (c = 'a'; c <= 'z'; c++)
+	{
 	_putchar(c);
-	{
-	_putchar('\n);
+	}
+	_putchar('\n');
 	i++;
 	}
 }
