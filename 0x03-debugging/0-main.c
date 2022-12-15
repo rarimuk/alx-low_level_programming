8cf5a7905d5cd8994f73a973fa52abc6419d0ef1
rarimuk <arimukruth@gmail.com>
2022-12-15 03:39:45 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative
+ * main - Tests function that prints if integer is positive or negative
  * Return: 0
  */
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-
+#ifndef MAIN_iH
 #define MAIN_H
 
 #include <stdio.h>
@@ -12,4 +11,4 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
 
-#endif
+#endif /* MAIN_H */
