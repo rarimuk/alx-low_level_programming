cb587bc7bac081ac7542e64a298aedae569e1d6e
rarimuk <arimukruth@gmail.com>
2023-01-05 13:56:54 -0800

    Task 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,7 +26,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));}
+	return (1 + _strlen_recursion(s + 1)); }
 
 /**
  * check_pal - checks the characters recursively for palindrome
